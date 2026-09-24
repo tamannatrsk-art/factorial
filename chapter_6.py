7287c531f93f0004568ce83e04d117ceddbfe2bd
Tamanna <tamannatrsk@gmail.com>
2026-09-24 17:33:41 +0600

    add ch_6

--- chapter_6.py
+++ chapter_6.py
@@ -4,7 +4,7 @@ print(spam[0])
 print(spam[1])
 print(spam[2])
 print("The" + spam[1] + "are the " + spam[0])
-for i in range(8):
+for i in range(8):  # try excpet
     try:
         if i >3:
             raise IndexError("Index is not here.")
@@ -17,7 +17,7 @@ print(another_list[0])
 print(another_list[0][1])
 print(another_list[1][4])
 
-for j in range(6):
+for j in range(6): # try excepct
     try:
         if j >2:
             raise IndexError("Index is not here.")
@@ -25,5 +25,27 @@ for j in range(6):
     except IndexError as e :
         print(e)
 
-del spam[2]
+del spam[2] # list delete
 print(spam)
+print(len(spam)) # list len
+spam[-1] = 12345 # list replace
+print(spam)
+
+
+
+
+print("Enter the of cat 1 : ")
+cat_name_1 = input()
+print("Enter the of cat 2 : ")
+cat_name_2 = input()
+print("Enter the of cat 3 : ")
+cat_name_3 = input()
+print("Enter the of cat 4 : ")
+cat_name_4 = input()
+print("The cat names are : ")
+print(cat_name_1 + ' ' + cat_name_2 + ' '+ cat_name_3 + ' ' + cat_name_4 +' ')
+
+
+
+for i in range(4):
+print(i)
