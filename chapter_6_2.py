2f8a448201c5afb756ef18d23c537f102287f29b
Tamanna <tamannatrsk@gmail.com>
2026-09-26 16:52:58 +0600

    Add matrix digital rain effect

---
+++ chapter_6_2.py
@@ -0,0 +1,20 @@
+import random,sys,time
+
+WIDTH = 70
+
+try:
+    columns = [0] * WIDTH
+    while True:
+        for i in range(WIDTH):
+            if random.random() < 0.02:
+                columns[i] = random.randint(4,14)
+
+            if columns[i] == 0:
+                print(" ",end = " ")
+            else:
+                print(random.choice([0,1]), end = " ")
+                columns[i] -= 1
+                
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    sys.exit()
